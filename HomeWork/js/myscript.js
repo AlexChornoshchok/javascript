92aad972f3b448b81c350afcd6b6fccecce1af05
User <aleksblacke@gmail.com>
2018-07-05 17:21:53 +0300

    Home work 3 july 2018

---
+++ HomeWork/js/myscript.js
@@ -0,0 +1,9 @@
+var a=Math.random()*10;
+a=a.toFixed();
+var b=Math.random()*10;
+b=b.toFixed();
+var c = Number();
+var d = Number();
+c=Math.cos(a)*(Math.sin(b)+1);
+d=(Math.tan(c)*Math.ctan(c))/100;
+alert("a= "+ a +"\n" + "b= "+ b + "\n" + "c= " + c +"\n" + "d= " + d);
